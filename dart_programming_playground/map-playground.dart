363c67481e11add417fbf0bfc81a119ae0917770
mahmudulislam299 <mahmudulislam299@gmail.com>
2022-04-14 12:39:42 +0600

    Map function added

--- dart_programming_playground/map-playground.dart
+++ dart_programming_playground/map-playground.dart
@@ -1,4 +1,9 @@
 void main() {
+  map_func1();
+  Map_func2();
+}
+
+void map_func1() {
   // map function
 
   // Double the all numbers in the list and print it
@@ -10,4 +15,15 @@ void main() {
   List<String> names = ['Max', 'Ed', 'Mahmud', 'peter'];
   var lengthName = names.map((name) => name.length);
   print(lengthName);
+}
+
+Map_func2() {
+  /// Map is like
+  /// Object literal in javascripts
+  /// dictionary in python
+
+  Map student = {'name': 'mahmud', 'age': 26};
+  print(student['name']);
+  print(student['age']);
+  print(student);
 }
